e81867a5b7c03e945330a01c12406608765d440e
Администратор <@DESKTOP-PJPICF5>
2022-01-08 15:49:51 +0800

    Добавьте файлы проекта.

---
+++ 9_Task_WPF/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _9_Task_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Тёмная тема" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedIndex = 0;
+        }
+
+    
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("White.xaml", UriKind.Relative);
+            if (styleIndex == 1)
+            {
+                uri = new Uri("Dark.xaml", UriKind.Relative);
+            }
+            ResourceDictionary resourse = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourse);
+            bool f = (bool)rbtnRed.IsChecked;
+            if (styleIndex == 0)
+            {
+                if (!f)
+                    textBox.Foreground = Brushes.Black;
+                else
+                    textBox.Foreground = Brushes.Red;
+            }
+            if (styleIndex == 1)
+            {
+                if (!f)
+                    textBox.Foreground = Brushes.White;
+                else
+                    textBox.Foreground = Brushes.Red;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = (sender as ComboBox).SelectedItem as String;
+            if (textBox!= null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+
+        }
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as String));
+
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSize;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+
+            else
+                textBox.FontWeight = FontWeights.Normal;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+
+            else
+                textBox.FontStyle = FontStyles.Normal;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == null)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+
+            else
+                textBox.TextDecorations = null;
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            int styleIndex = styleBox.SelectedIndex;
+            if (styleIndex == 1)
+                textBox.Foreground = Brushes.White;
+            if (styleIndex == 0)
+                textBox.Foreground = Brushes.Black;
+        }
+
+        private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            textBox.Foreground = Brushes.Red;
+        }
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы(*.txt)|*.txt|Все файлы(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы(*.txt)|*.txt|Все файлы(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ 9_Task_WPF/MyCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _9_Task_WPF
+{
+    internal class MyCommands
+    {
+        public static RoutedUICommand Exit { get; set; }
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.F4, ModifierKeys.Alt, "Alt+F4"));
+            Exit = new RoutedUICommand("Exit", "Exit", typeof(MyCommands), inputs);
+        }
+    }
+}
